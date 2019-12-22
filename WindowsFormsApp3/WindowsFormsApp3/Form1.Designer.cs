7f833fe88ce0089b5fa3302464d4483e8f97ded2
Unknown <dolmatovdan2006@gmail.com>
2019-12-22 13:09:23 +0300

    All proyect

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(91, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1-ое число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(284, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2-ое число";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(464, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3-е число";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(631, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4-ое число";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(316, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate numbers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(316, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 71);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Get numbers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void showNumbers(int[] numbers)
+        {
+            string name = "label";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Control ctn = this.Controls[name + (i + 1)];
+                if (ctn != null)
+                {
+                    ctn.Text = numbers[i].ToString();
+                }
+            }
+
+        }
+        public int[] getNumbers()
+        {
+            string name = "label";
+            int[] numbers = new int[4];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Control ctn = this.Controls[name + (i + 1)];
+                if (ctn != null)
+                {
+                    numbers[i] = Convert.ToInt32(ctn.Text);
+                }
+            }
+            return numbers;
+        }
+        public static int[] name(int[] arr)
+        {
+            Random random = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = random.Next(0, 251);
+                Console.WriteLine(arr[i]);
+            }
+            name(arr);
+            Console.WriteLine("Правильный порядок");
+
+
+            Console.ReadLine();
+            return array;
+        }
+        public static int[] sort(int[] array)
+        {
+            string s = "";
+            for (int z = 0; z < array.Length; z++)
+            {
+                for (int a = 0; a < array.Length - 1; a++)
+                {
+                    int b = array[a];
+                    if (array[a] > array[a + 1])
+                    {
+                        int d = array[a];
+                        array[a] = array[a + 1];
+                        array[a + 1] = d;
+                    }
+                }
+            }
+            
+            return array;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] numbs = new int[4];
+            Random rnd = new Random();
+            for (int b = 0; b < 4; b++)
+            {
+                numbs[b] = rnd.Next(0, 251);
+            }
+            this.showNumbers(numbs);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            showNumbers(sort(getNumbers()));
+        }
+    }
+}
+
